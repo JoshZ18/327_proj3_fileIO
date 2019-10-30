8c65032c28fb12325c3a9ebd8cc3b8a408a61d89
Josh Zutell <joshua.zutell.18@cnu.edu>
2019-10-30 17:55:53 -0400

    Add comments

--- FileIO.cpp
+++ FileIO.cpp
@@ -22,12 +22,16 @@ int KP_FileIO::getFileContents(const std::string &filename, std::string &content
 {
 	std::string file = filename;
 
+	//Attempts to open a file to read
 	ifstream inputFile;
 	inputFile.open(file.c_str());
 
+	//Checks to see if the file is open
 	if (inputFile.is_open()) {
+		//Reads until the end of the file
 		while (!inputFile.eof()) {
 			std::string line;
+			//Gets a line from the file
 			getline(inputFile, line);
 
 			int length = line.length();
@@ -35,16 +39,18 @@ int KP_FileIO::getFileContents(const std::string &filename, std::string &content
 			//Loops through every line in the file and parses out data
 			for (int i = 0; i < length; i++) {
 				std::string character = line.substr(i, 1);
-				//Separates pieces by data based on ,
+				//Adds contents to a string except if it is \n
 				if (character != "\n") {
 					contents += character;
 				}
 			}
 		}
+		//Closes the file and returns success
 		inputFile.close();
 		return SUCCESS;
 	}
 
+	//Returns could not open file to read if the file is not open
 	return COULD_NOT_OPEN_FILE_TO_READ;
 }
 
@@ -54,17 +60,22 @@ int KP_FileIO::getFileContents(const std::string &filename, std::string &content
 //COULD_NOT_OPEN_FILE_TO_WRITE
 int KP_FileIO::writeVectortoFile(const std::string filename, std::vector<std::string> &myEntryVector)
 {
+	//Attempts to open a file to write
 	ofstream outputFile;
 	std::string file = filename;
 	outputFile.open(file.c_str());
 
+	//Checks if the file is open
 	if (outputFile.is_open()) {
+		//Adds the contents of the vector into the file
 		for (int i = 0; i < myEntryVector.size(); i++) {
 			outputFile << myEntryVector[i];
 		}
+		//Closes the file and returns success
 		outputFile.close();
 		return SUCCESS;
 	}
 
+	//Returns could not open file to write if the file is not open
 	return COULD_NOT_OPEN_FILE_TO_WRITE;
 }
